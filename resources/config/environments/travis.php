0680854377de75b47d3b84e7a710c96d9e628de4
Lewis Dyer <signup@icomefromthenet.com>
2013-01-02 16:40:41 +1100

    add missing env file for travis-ci

---
+++ resources/config/environments/travis.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * Development Config File
+ *
+ */
+
+# ----------------------------------------------------
+# Set Silex to Debug mode
+# 
+# ---------------------------------------------------
+
+$app["debug"] = true;
+
+
+# ----------------------------------------------------
+# Set Monolog
+# ---------------------------------------------------
+
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => $app['cache_path'].'logs/quicktag.log',
+));
+
+# ----------------------------------------------------
+# Log queries into a file
+# ---------------------------------------------------
+
+$app->before(function() use ($app) {
+   
+   $logger = new DBALGateway\Feature\StreamQueryLogger($app['monolog']);
+   
+   $app['dispatcher']->addSubscriber($logger);
+    
+});
+# ----------------------------------------------------
+# Setup Database and PDOSessionHandler
+# 
+# ---------------------------------------------------
+
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'    => 'pdo_mysql',
+        'host'      => 'localhost',
+        'dbname'    => 'quick_tag',
+        'user'      => 'root',
+        'password'  => null,
+    )
+));
